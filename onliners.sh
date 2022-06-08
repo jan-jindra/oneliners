1b5c44e10996a52109cf2421ed7570f6d0c59aa1
jan-jindra <jindra@lumacomp.cz>
2022-06-08 07:29:51 +0200

    import ssh key update

--- onliners.sh
+++ onliners.sh
@@ -1,2 +1,3 @@
 #ssh key import
-wget https://it-test.eu/LUMA/pernament/john.pub && mkdir ~/.ssh/ && cat john.pub >> ~/.ssh/authorized_keys
+wget https://it-test.eu/LUMA/pernament/john.pub && mkdir ~/.ssh/ 
+cat john.pub >> ~/.ssh/authorized_keys && rm john.pub
